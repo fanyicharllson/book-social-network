d7e4e8fb8a2432d7151f82ac8a7eb41927c3b24c
fanyicharllson <fanyicharllson1@gmail.com>
2025-07-04 15:36:43 -0700

    Added user and role entity

--- src/main/java/com/book_network/spring/boot/book_social_network/BookNetworkApplication.java
+++ src/main/java/com/book_network/spring/boot/book_social_network/BookNetworkApplication.java
@@ -2,8 +2,10 @@ package com.book_network.spring.boot.book_social_network;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookNetworkApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/book_network/spring/boot/book_social_network/user/User.java
+++ src/main/java/com/book_network/spring/boot/book_social_network/user/User.java
@@ -1,5 +1,115 @@
 package com.book_network.spring.boot.book_social_network.user;
 
-public class User {
+import java.security.Principal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.book_network.spring.boot.book_social_network.role.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Represents a user in the system, implementing Spring Security's UserDetails and Principal interfaces.
+ * This class is used for authentication and authorization purposes.
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails, Principal {
+
+        @Id
+        @GeneratedValue
+        private Integer id;
+        private String firstName;
+        private String lastName;
+        private LocalDate dateOfBirth;
+        @Column(unique = true, nullable = false)
+        private String email;
+        private String password;
+        private boolean accountLocked;
+        private boolean enabled;
+
+        @ManyToMany(fetch = FetchType.EAGER)    
+        private List<Role> roles;
+
+        @CreatedDate
+        @Column(nullable = false, updatable = false)
+        private LocalDateTime createdDate;
+
+        @LastModifiedDate
+        @Column(insertable = false)
+        private LocalDateTime lastModifiedDate;
+
+
+    @Override
+    public String getName() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+       return this.roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }       
+
+    @Override
+    public String getPassword() {
+       return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
     
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; 
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    private String fullName() {
+        return firstName + " " + lastName;
+    }
 }
